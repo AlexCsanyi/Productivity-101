140f792be79b7add2511c170e0d3bb1a198f9c44
AlexCsanyi <alex.csanyi@gmail.com>
2019-10-02 06:44:20 +0100

    added react-router, built routing and nav, added logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import TodosList from "./components/todos-list.component";
+import EditTodo from "./components/edit-todo.component";
+import CreateTodo from "./components/create-todo.component";
 import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "./imgs/pro101-logo.png";
 
 function App() {
   return (
-    <div className="container">
-      <h2>MERN Productivity App</h2>
-    </div>
+    <Router>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="alexcsanyi.me" target="_blank">
+            <img
+              src={logo}
+              width="50"
+              height="50"
+              alt="Productivity 101 app"
+            ></img>
+          </a>
+          <Link to="/" className="navbar-brand">
+            PRO 101
+          </Link>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="navbar-item">
+                <Link to="/" className="nav-link">
+                  ToDos
+                </Link>
+              </li>
+              <li className="navbar-item">
+                <Link to="/create" className="nav-link">
+                  Create
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/create-todo.component.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class CreateTodo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Welcome to the Create Todos Component</p>
+      </div>
+    );
+  }
+}

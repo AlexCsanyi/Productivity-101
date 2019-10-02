ec82b943d21139c2d7b2955c69ee57b34b644e50
AlexCsanyi <alex.csanyi@gmail.com>
2019-10-02 06:50:18 +0100

    added state to create component and first basic methods

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -12,6 +12,35 @@ export default class CreateTodo extends Component {
     };
   }
 
+  onChangeTodoDescription(e) {
+    this.setState({
+      todo_description: e.target.value
+    });
+  }
+
+  onChangeTodoResponsible(e) {
+    this.setState({
+      todo_responsible: e.target.value
+    });
+  }
+
+  onChangeTodoPriority(e) {
+    this.setState({
+      todo_priority: e.target.value
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.setState({
+      todo_description: "",
+      todo_responsible: "",
+      todo_priority: "",
+      todo_completed: false
+    });
+  }
+
   render() {
     return (
       <div>
